2900a5130fc6236a7e41f143554fb3c75a978bd8
Luanne Teles de Oliveira <2301320@salas.aulas>
2026-04-06 20:12:19 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"msg": "API rodando"}
+
+@app.get("/somar/{a}/{b}")
+def somar(a: int, b: int):
+    return {"resultado": a + b}
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import somar
+
+def test_somar():
+    assert somar(2, 2)["resultado"] == 4
